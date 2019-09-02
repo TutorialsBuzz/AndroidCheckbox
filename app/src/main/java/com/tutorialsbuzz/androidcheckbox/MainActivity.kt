542ece1151c675bfc9f7edecae0236791259451f
Pawan Deshpande <Pawan.Deshpande@ril.com>
2019-09-02 08:11:13 +0530

    Code Update

--- app/src/main/java/com/tutorialsbuzz/androidcheckbox/MainActivity.kt
+++ app/src/main/java/com/tutorialsbuzz/androidcheckbox/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.tutorialsbuzz.androidcheckbox
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.TypedValue
-import android.view.ViewGroup
-import android.widget.CheckBox
 import android.widget.Toast
-import androidx.core.view.setPadding
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,19 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         checkbox01.setOnCheckedChangeListener({ buttonView, isChecked ->
-            Toast.makeText(this@MainActivity, "" + isChecked, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@MainActivity, buttonView.text.toString()+ ": " + isChecked, Toast.LENGTH_SHORT).show()
         })
 
-        val checkBox = CheckBox(this)
-        checkBox.layoutParams =
-            ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        checkBox.setText("CheckBox")
-        checkBox.setPadding(24)
-        checkBox.setTextSize(TypedValue.COMPLEX_UNIT_SP,15f)
-        root.addView(checkBox)
-
-
+        checkbox02.setOnCheckedChangeListener({ buttonView, isChecked ->
+            Toast.makeText(this@MainActivity, buttonView.text.toString()+ ": " + isChecked, Toast.LENGTH_SHORT).show()
+        })
     }
 }
